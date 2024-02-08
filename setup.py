3193631530522b8387d2e77248b7513c2e44a497
Michael Howitz <icemac@gmx.net>
2024-02-08 12:09:02 +0100

    Add support for Python 3.12.

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+version = '2.1.dev0'
+
+with open('README.rst') as f:
+    README = f.read()
+
+setup(
+    name='zopetoolkit',
+    version=version,
+    description='Set of libraries maintained by the Zope project.',
+    author='Zope team',
+    author_email='zope-dev@zope.dev',
+    license='ZPL',
+    long_description_content_type='text/x-rst',
+    long_description=README,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Zope Public License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    zip_safe=False,
+    python_requires='>=3.7',
+    install_requires=[],
+    entry_points={},
+    packages=[],
+    )
